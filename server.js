587a48076ddffe010e69a9cdde913f4b78f27415
Ali Nguyen <alin57717@gmail.com>
2020-10-01 17:05:50 -0700

    set up express for data parsing

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const expres = require ("express");
 const fs = require("fs");
 const path = require("path");
 
-const app = expres();
+const app = express();
 const PORT = process.env.PORT|| 3000;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname));
