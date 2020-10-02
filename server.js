562a4e18933f773d4e86e1b03ef26961a41c0e7c
Ali Nguyen <alin57717@gmail.com>
2020-10-01 18:02:53 -0700

    create route folder to store route script

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const expres = require ("express");
+const express = require ("express");
 const fs = require("fs");
-const path = require("path");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT|| 3000;
