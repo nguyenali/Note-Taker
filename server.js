d3dccc4d05149a634770df38e73bd4b02d86569d
Ali Nguyen <alin57717@gmail.com>
2020-10-01 17:12:14 -0700

    routes file and listner on PORT

--- server.js
+++ server.js
@@ -8,3 +8,11 @@ const PORT = process.env.PORT|| 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(__dirname));
+
+
+require('./routes/routes')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT:" + PORT);
+})
+
